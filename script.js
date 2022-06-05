e167f6227ef86ed0be19dacce68bd97b8ecd9d6f
Lov3Life <106180858+Lov3Life@users.noreply.github.com>
2022-06-05 17:44:11 +0200

    close link script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const OpenButton = document.getElementById("open-button");
 const CloseButton = document.getElementById("close-button");
 const NavBar = document.querySelector("nav");
 const MainStyle = document.querySelector("main");
+const CloseLink = document.getElementsByClassName("close-link");
 
 OpenButton.addEventListener("click", NavBarDisplay);
 CloseButton.addEventListener("click", NavBarDisplayNone);
@@ -18,4 +19,12 @@ function NavBarDisplayNone() {
     MainStyle.style.marginLeft = "0";
     OpenButton.style.display = "inline";
     OpenButton.style.margin = "5px";
+}
+
+window.onload = HowManyCloseLink;
+
+function HowManyCloseLink() {
+    for(let i = 0; i < CloseLink.length; i++) {
+        CloseLink[i].addEventListener("click", NavBarDisplayNone);
+    }
 }
